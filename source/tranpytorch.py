4b85d0e04b3a95df0eb61ea83d0619e6e0ecd101
Tai nguyen van <nvt.kscntt@gmail.com>
2019-01-09 00:20:29 +0700

    "update use cuda"

--- source/tranpytorch.py
+++ source/tranpytorch.py
@@ -443,7 +443,7 @@ def general_nfold_cv(XD, XT, Y, label_row_inds, label_col_inds, prfmeasure, runm
                     print("param ",param2value," ",param1value," ",param3value)
                     model.cuda()
                     criterion = nn.MSELoss()
-                    optimizer = optim.Adam(model.parameters(),lr=0.0005)
+                    optimizer = optim.Adam(model.parameters(),lr=0.06)
                     for i in range(epoch):
                         loss_epoch=0
                         model.train()
